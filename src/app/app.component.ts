44cddb179dd1a16f4b0a1af8d025a64155ccece1
eddsanchez <eddcsanchez@gmail.com>
2018-10-23 09:21:03 -0500

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,16 @@ import { stringify } from 'querystring';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listatareas';
   tareas: Tarea[]; //Arreglo de tareas
   ultimoId: number;
+  //  tareasCompletadas: number;
 
   constructor() {
     this.tareas = [
       { id: 1, titulo: 'Terminar el API', completada: false },
-      { id: 2, titulo: 'Corregir historias de usuario', completada: true },
-      new Tarea({ id: 3, titulo: 'Comprar entradas para el cine', completada: false })
-    ]
+      { id: 2, titulo: 'Corregir Historias de Usuario', completada: true },
+      new Tarea({ id: 3, titulo: 'Comprar boletos para el cine' })
+    ];
     this.ultimoId = 3;
   }
 
@@ -25,14 +25,39 @@ export class AppComponent {
     tareaNueva.id = ++this.ultimoId;
     this.tareas.push(tareaNueva);
   }
+
+  eliminarTarea(idTarea: number) {
+    this.tareas = this.tareas.filter(function (tarea) {
+      return tarea.id !== idTarea;
+    });
+  }
+
+  toggleTarea(idTarea) {
+    for (const tarea of this.tareas) {
+      if (tarea.id === idTarea) {
+        tarea.completada = !tarea.completada;
+        return;
+      }
+    }
+  }
+  // actualizarTareasCompletadas() {
+  tareasCompletadas(): number {
+    console.log('Actualizando tareas...');
+    // this.tareasCompletadas = this.tareas.filter(function(tarea) {
+    return this.tareas.filter(function (tarea) {
+      return tarea.completada === true;
+    }).length;
+    // console.log('Tareas completadas = ' + this.tareasCompletadas);
+  }
 }
 
 class Tarea {
   id: number;
   titulo: string;
-  completada: boolean
+  completada: boolean;
 
   constructor(valores: Object = {}) {
     Object.assign(this, valores);
   }
 }
+
